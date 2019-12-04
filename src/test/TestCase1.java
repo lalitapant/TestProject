2ae3b46c378f12f7a9e0a69e2b3e415e5e00cc2e
User <User@192.168.0.11>
2019-12-04 12:08:01 -0500

    First Commit

---
+++ src/test/TestCase1.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestCase1 
+{
+	public static void main(String args[])throws IOException
+	
+	{
+	Properties prop=new Properties();
+	FileInputStream fs=new FileInputStream("C:\\Users\\User\\Desktop\\Learning\\Java-PropertiesExcel\\src\\config\\object.properties");
+	prop.load(fs);
+	System.out.println(prop.getProperty("name"));
+	}
+}
