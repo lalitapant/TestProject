fbd05d1ee5f00a17579e822c8b9cc1dab373d19f
lalitapant <lalita.asm@gmail.com>
2021-05-27 23:13:16 -0400

    Update TestCase1.java

--- src/test/TestCase1.java
+++ src/test/TestCase1.java
@@ -10,6 +10,7 @@ public class TestCase1
 	public static void main(String args[])throws IOException
 	
 	{
+		//Commit new changes
 	Properties prop=new Properties();
 	FileInputStream fs=new FileInputStream("C:\\Users\\User\\Desktop\\Learning\\Java-PropertiesExcel\\src\\config\\object.properties");
 	prop.load(fs);
